45134e4830bc20f382fc05820a3de16b83535e66
dikshasingh-1 <diksha.singh2025@glbajajgroup.org>
2026-07-06 20:07:06 +0530

    DAY8

---
+++ repeated number pattern.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+    if (scanf("%d", &rows) != 1) return 1;
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+    return 0;
+}

604da72bca9c73f23c541638851d256b9c24443d
dikshasingh-1 <diksha.singh2025@glbajajgroup.org>
2026-07-07 22:51:19 +0530

    DAY23

---
+++ find max occurring character.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    char maxChar;
+    int max = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] != '\n')
+            freq[(unsigned char)str[i]]++;
+    }
+
+    for(int i = 0; i < 256; i++) {
+        if(freq[i] > max) {
+            max = freq[i];
+            maxChar = i;
+        }
+    }
+
+    printf("Maximum occurring character: %c", maxChar);
+    return 0;
+}
